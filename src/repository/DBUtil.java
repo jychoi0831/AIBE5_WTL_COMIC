36b75e9525cf18963a264cc93d58beb895c412dd
sdg3729 <yu760923@naver.com>
2026-03-20 09:12:44 +0900

    Update DBUtil with placeholder credentials

--- src/repository/DBUtil.java
+++ src/repository/DBUtil.java
@@ -8,8 +8,8 @@ public class DBUtil {
 	
 	// DB 접속 정보
 	private static final String URL = "jdbc:mysql://localhost:3306/comic_rental?serverTimezone=Asia/Seoul";
-    private static final String USER = "root";        // 본인 MySQL 계정으로 수정
-    private static final String PASSWORD = "1234";    // 본인 MySQL 비밀번호로 수정
+    private static final String USER = "root"; 
+    private static final String PASSWORD = "여기에_본인_mysql_비밀번호"; // 여기에 본인 MySQL 비밀번호
 
     public static Connection getConnection() throws SQLException {
         try {
